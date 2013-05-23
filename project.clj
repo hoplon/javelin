f364f2c44525f3d7b6b9fff9f422204baf07ff3f
Alan Dipert <alan@dipert.org>
2013-05-22 20:53:54 -0400

    bump priority-map to 1.0.1 and enjoy perf boost

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/tailrecursion/javelin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[tailrecursion/cljs-priority-map "1.0.0"]
+  :dependencies [[tailrecursion/cljs-priority-map "1.0.1"]
                  [alandipert/desiderata "1.0.1"]]
   :plugins [[lein-cljsbuild "0.3.0"]]
   :source-paths ["src/clj" "src/cljs"]
