28015c0cfe7a1d4e00470c55033643f859a4263b
Micha Niskin <asdf@ubergibson.com>
2013-10-16 12:20:51 -0400

    release v2.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tailrecursion/javelin "2.0.0-SNAPSHOT"
+(defproject tailrecursion/javelin "2.0.0"
   :description "A Functional Reactive Programming library for ClojureScript"
   :url "https://github.com/tailrecursion/javelin"
   :license {:name "Eclipse Public License"
